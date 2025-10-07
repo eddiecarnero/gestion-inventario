a6a309b1c3e53787866b5cc9913c6d686d67dbe5
CuentaSinNombre <eddie.carnero@unmsm.edu.pe>
2025-10-06 20:03:43 -0500

    UI LoginPage

--- src/com/Main.java
+++ src/com/Main.java
@@ -1,5 +1,26 @@
 package com;
 
-public class Main {
+import javax.swing.*;
 
+public class Main extends JFrame {
+    private JPanel panel1;
+    private JTextField ingreseSuUsuarioTextField;
+    private JPasswordField passwordField1;
+    private JButton iniciarSesionButton;
+    private JLabel imageCafe;
+
+    public Main() {
+        setContentPane(panel1);
+        setTitle("Gestion Inventario");
+        setSize(1020, 640);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setResizable(false);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            new Main().setVisible(true);
+        });
+    }
 }
