85167d97657db8d5bec2800f9f37d121f0f354aa
CuentaSinNombre <eddie.carnero@unmsm.edu.pe>
2025-10-17 23:08:42 -0500

    UI LoginPage

--- src/com/inventario/util/ConexionDB.java
+++ src/com/inventario/util/ConexionDB.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class ConexionDB {
     private static final String URL = "jdbc:mysql://localhost:3306/mamatania";
     private static final String USER = "root";
-    private static final String PASSWORD = "CLL2606ec";
+    private static final String PASSWORD = "root";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
