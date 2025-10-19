d1f272232e1be16ca150a4f6a2c94d99d55fda88
Yesenia Alessandra Angeles Reyes <yesenia.angeles@unmsm.edu.pe>
2025-10-19 18:02:51 -0500

    fix(config): Conexión a BD en la nube configurada y probada.

--- src/com/inventario/config/ConexionBD.java
+++ src/com/inventario/config/ConexionBD.java
@@ -5,11 +5,18 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConexionBD {
-    private static final String URL = "jdbc:mysql://localhost:3306/inventario";
+    private static final String URL = "jdbc:mysql://mainline.proxy.rlwy.net:22473/railway";
     private static final String USER = "root";
-    private static final String PASSWORD = "1234";
+    private static final String PASSWORD = "ePJWIUfTkvcokoIfdHAekzLPBezJxkdf";
 
     public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver"); // Carga explícita del driver
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("Driver MySQL no encontrado: " + e.getMessage());
+        }
+
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
+
 }
